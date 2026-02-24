db4f9052381a21c8dd15627b0feadd1c8c9d3894
Ji Younghyun <younghyun1@gmail.com>
2026-02-24 07:29:45 -0700

    feature: zstd reintroduced

--- deploy_to_be.sh
+++ deploy_to_be.sh
@@ -2,11 +2,11 @@
 npm run build
 rm -rf ../rust-be-template/fe/*
 find ./dist -type f ! -iname '*.png' ! -iname '*.jpg' \
-  -exec sh -c 'gzip -9 -c "$1" > "$1.gz"' _ {} \;
+  -exec sh -c 'zstd --ultra -22 -c "$1" > "$1.zst"' _ {} \;
 
 cd ./dist
-# Copy gzip'd files
-find . -type f -name '*.gz' | while read file; do
+# Copy zstd-compressed files
+find . -type f -name '*.zst' | while read file; do
   mkdir -p "../../rust-be-template/fe/$(dirname "$file")"
   cp "$file" "../../rust-be-template/fe/$file"
 done
