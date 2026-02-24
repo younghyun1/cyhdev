e5d5de65dc2d3926f82ab150af027c51c9fecf94
Ji Younghyun <younghyun1@gmail.com>
2026-02-24 07:50:35 -0700

    feature: zstd reintroduced; gzip optional

--- deploy_to_be.sh
+++ deploy_to_be.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 npm run build
 rm -rf ../rust-be-template/fe/*
-find ./dist -type f ! -iname '*.png' ! -iname '*.jpg' \
-  -exec sh -c 'zstd --ultra -22 -c "$1" > "$1.zst"' _ {} \;
+find ./dist -type f ! -iname '*.png' ! -iname '*.jpg' ! -iname '*.gz' ! -iname '*.zst' \
+  -exec sh -c 'gzip -9 -c "$1" > "$1.gz"; zstd --ultra -22 -c "$1" > "$1.zst"' _ {} \;
 
 cd ./dist
-# Copy zstd-compressed files
-find . -type f -name '*.zst' | while read file; do
+# Copy compressed files
+find . -type f \( -name '*.gz' -o -name '*.zst' \) | while read file; do
   mkdir -p "../../rust-be-template/fe/$(dirname "$file")"
   cp "$file" "../../rust-be-template/fe/$file"
 done
