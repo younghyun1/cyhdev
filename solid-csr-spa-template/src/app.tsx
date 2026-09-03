16c31f4ce49b5b600d02189121bfa7a5a08d445f
Young Hyun Chi <younghyun1@gmail.com>
2026-09-03 02:02:59 -0600

    fix: keep content above status bar

--- solid-csr-spa-template/src/app.tsx
+++ solid-csr-spa-template/src/app.tsx
@@ -84,7 +84,7 @@ const App: ParentComponent = (props) => {
         >
           <TopBar />
 
-          <main class="flex-1 min-h-0 pb-10 pt-12 sm:pt-14">
+          <main class="site-main flex-1 min-h-0 pt-12 sm:pt-14">
             <Errored
               fallback={(err, reset) => {
                 const message = () => {
